62063515d2ae52b91fd5e512f8795e5c00ab94a9
farrukh0913 <farrukh.khan0913@gmail.com>
2025-05-08 19:52:52 +0500

    Initial commit

---
+++ src/datasources/db.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'db',
+  connector: 'mysql',
+  host: 'localhost', // or '127.0.0.1'
+  port: 3306, // default MySQL port
+  user: 'root', // your MySQL username
+  password: 'Test1234!', // your MySQL password
+  database: 'main',
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class DbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
---
+++ src/models/company.model.ts
@@ -0,0 +1,50 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Company extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  companyId?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  companyName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+  })
+  phone?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  rate: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Company>) {
+    super(data);
+  }
+}
+
+export interface CompanyRelations {
+  // describe navigational properties here
+}
+
+export type CompanyWithRelations = Company & CompanyRelations;
